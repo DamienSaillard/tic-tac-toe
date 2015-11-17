f4fda7614c21bc006e6ab091572319e6490450d1
DamienSaillard <damiensaillard92@gmail.com>
2015-11-17 14:23:50 +0100

    step 4

--- lib/app.jsx
+++ lib/app.jsx
@@ -12,11 +12,15 @@ var Box = React.createClass({
    * Render a HTML button
    * @return {ReactElement}
    */
+   getInitialState: function() {
+	return {value: this.props.initialValue}
+   },
+   
   'render': function onRender () {
     return (
-      <button style={ButtonStyle}>{this.props.value}</button>
+      <button style={ButtonStyle}>{this.state.value}</button>
     );
   }
 });
 
-React.render(<Box value='X'/>, document.body);
+React.render(<Box initialValue='O'/>, document.body);
