c10a0c52f039573872f8fe71544e7c2282e4c648
DamienSaillard <damiensaillard92@gmail.com>
2015-11-17 15:17:04 +0100

    step 5

--- lib/app.jsx
+++ lib/app.jsx
@@ -15,10 +15,30 @@ var Box = React.createClass({
    getInitialState: function() {
 	return {value: this.props.initialValue}
    },
+    
+	componentWillMount: function (){
+	setInterval(this.updatetime,300);
+	},
+	updatetime: function()
+	{
+	if(this.state.value=='X') this.setState({value:'O'});
+	else this.setState({value:'X'});
+	},
+	
+	
+   /**
+   handleClick: function() {
+   var etat = this.state.value;
+   var newetat;
+   if (etat == 'X') newetat='O';
+   else newetat='X';
+   this.setState({value:newetat});
+   }, */
    
+
   'render': function onRender () {
     return (
-      <button style={ButtonStyle}>{this.state.value}</button>
+      <button style={ButtonStyle} onClick={this.handleClick}>{this.state.value}</button>
     );
   }
 });
